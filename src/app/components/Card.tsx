df6a2426a9faf89fadcfc1776ed998d341db85d3
ahmodu4real89 <ahmodu4real89@gmail.com>
2025-07-26 13:02:34 +0100

    new commit

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,29 +1,3 @@
-// interface CardProps {
-//   title: string;
-//   description: string;
-//   buttonText: string;
-//   color: string; 
-//   backgroundColor:string
-//   buttonTextColor:string
-//   textColor:string
-// }
-
-// const Card = ({ title, description, buttonText, color, backgroundColor, buttonTextColor, textColor }: CardProps) => {
-//   return (
-//     <div className={`${backgroundColor} ${ textColor} shadow-sm rounded-sm p-4 `}>
-//       <h3 className="text-base font-semibold  mb-2">{title}</h3>
-//       <p className="text-sm  mb-8">{description}</p>
-//       <button
-//         className={`${color} ${buttonTextColor} text-sm  px-4 py-4 rounded-md hover:opacity-90 transition w-full `}
-//       >
-//         {buttonText}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Card;
-
 
 interface CardProps {
   title: string;
@@ -46,7 +20,7 @@ const Card = ({
 }: CardProps) => {
   return (
     <div
-      className={`${backgroundColor} ${textColor} shadow-sm rounded-md p-4 sm:p-6 flex flex-col justify-between h-full`}
+      className={`${backgroundColor} ${textColor} shadow-sm rounded-md p-4 sm:p-6 flex flex-col justify-between h-full mb-12`}
     >
       <div>
         <h3 className="text-base sm:text-lg font-semibold mb-2">{title}</h3>
