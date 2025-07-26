0e7f363f63165d9445aff0fe8e42e26ee4e39d21
ahmodu4real89 <ahmodu4real89@gmail.com>
2025-07-26 12:58:33 +0100

    new commit

--- src/app/components/Tabs.tsx
+++ src/app/components/Tabs.tsx
@@ -2,10 +2,10 @@
 
 const Tabs = () => {
   return (
-    <>    
-          <p className="py-2 font-bold text-xl"> Trip itineraries</p>
+    <div>    
+          <p className=" font-bold text-xl"> Trip itineraries</p>
           <p className="text-sm text-gray-500">Your trip itineraries are placed here</p>  
-    </>
+    </div>
   )
 }
 
